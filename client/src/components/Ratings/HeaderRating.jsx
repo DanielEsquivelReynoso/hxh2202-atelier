9c043bc52be7be9ed640ceeb4a799dacfc3f9682
DanielEsquivelReynoso <daniel.esquivelreynoso@gmail.com>
2022-10-21 10:19:21 -0700

    cleared up excess console.logs

--- client/src/components/Ratings/HeaderRating.jsx
+++ client/src/components/Ratings/HeaderRating.jsx
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import Stars from './Stars.jsx';
 import Ratings from './Ratings.jsx';
 
-// import { averageRating, ones } from './oneMeta.js';
-
 const HeaderRatingContainer = styled.div`
   display: inline-grid;
   grid-template-columns: 2fr 1fr 1fr
@@ -25,27 +23,24 @@ const NumberStyle = styled.div`
 
 var ratingAverage = (distribution) => {
   const data = {};
-    let total = 0;
-    let weightedSum = 0;
+  let total = 0;
+  let weightedSum = 0;
 
-    for (let i = 0; i < 5; i++) {
-      const distributionValue = distribution?.[i + 1] || "0";
-      data[i + 1] = distributionValue;
-      total = total + parseInt(distributionValue);
-      weightedSum = weightedSum + (parseInt(distributionValue) * (i+1))
-    }
-    let avgRating = weightedSum / total;
+  for (let i = 0; i < 5; i++) {
+    const distributionValue = distribution?.[i + 1] || "0";
+    data[i + 1] = distributionValue;
+    total = total + parseInt(distributionValue);
+    weightedSum = weightedSum + (parseInt(distributionValue) * (i+1))
+  }
+  let avgRating = weightedSum / total;
 
-
-    return avgRating;
+  return avgRating;
 }
 
 const HeaderRating = ({ distribution, setStarsIndex }) => {
 
-
   const average = ratingAverage(distribution);
   const averageRounded = Math.round(average * 100) / 100
-
 
   return (
     <div>
